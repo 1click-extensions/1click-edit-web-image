7a1e1be51d0520552f20642be28e13d0fe941dac
Itsick <any.site.n.site@gmail.com>
2018-06-17 23:03:44 +0300

    added popup and add version number

--- js/show-popup.js
+++ js/show-popup.js
@@ -31,6 +31,7 @@ function oneClickGetPopupHtml(extension) {
 .pleaseRate a:hover,
 .pleaseRate a:visited{
 	color:#000;
+	text-decoration:underline;
 }
 .please-rate-text {
     margin: 0 auto;
@@ -110,7 +111,7 @@ button.btn-popup.no-thanks {
 
 		<div class="please-rate-text">
 			To advance the open-source world, and to give us motivation, 
-			If you like 1click edit web image <br/><a target=_blank href="https://chrome.google.com/webstore/detail/bjeeckhifeaoojlelpfcbiidfdlbognh/reviews" >please give us 5-stars</a>
+			If you like 1Click edit web image <br/><a target=_blank href="https://chrome.google.com/webstore/detail/bjeeckhifeaoojlelpfcbiidfdlbognh/reviews" >please give us 5-stars</a>
 			<br/>
 			<div class="addition">
 				In addition, If you want to report a bug, or you have a recommendation, please <a href="https://github.com/1click-extensions/1click-edit-web-image">report a public issue</a> or  <a href="mailto:1click-edit-web-image@1ce.org">Contact us</a>
